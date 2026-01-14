34e3133f7395aee8ded35d12ccf004c186f47d88
João Vitor <jvitoroliveira0711@gmail.com>
2026-01-14 20:10:34 -0300

    feat: Adiciona generic para a SinglyLinkedList

--- crates/singly-linked-list/src/lib.rs
+++ crates/singly-linked-list/src/lib.rs
@@ -1,26 +1,26 @@
-pub struct SinglyLinkedListNode {
-    value: i32,
-    next: Option<Box<SinglyLinkedListNode>>,
+pub struct SinglyLinkedListNode<T> {
+    value: T,
+    next: Option<Box<SinglyLinkedListNode<T>>>,
 }
 
-impl SinglyLinkedListNode {
-    pub fn new(value: i32, next: Option<Box<SinglyLinkedListNode>>) -> Self {
+impl<T> SinglyLinkedListNode<T> {
+    pub fn new(value: T, next: Option<Box<SinglyLinkedListNode<T>>>) -> Self {
         Self { value, next }
     }
 }
 
-pub struct SinglyLinkedList {
-    head: Option<Box<SinglyLinkedListNode>>,
+pub struct SinglyLinkedList<T> {
+    head: Option<Box<SinglyLinkedListNode<T>>>,
     counter: usize,
 }
 
-impl Default for SinglyLinkedList {
+impl<T> Default for SinglyLinkedList<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl SinglyLinkedList {
+impl<T> SinglyLinkedList<T> {
     pub fn new() -> Self {
         Self {
             head: Option::None,
@@ -36,19 +36,19 @@ impl SinglyLinkedList {
         self.counter == 0
     }
 
-    pub fn peek_front(&self) -> Option<&i32> {
+    pub fn peek_front(&self) -> Option<&T> {
         self.head.as_deref().map(|n| &n.value)
     }
 
-    pub fn push_front(&mut self, value: i32) {
+    pub fn push_front(&mut self, value: T) {
         let old_head = self.head.take();
-        let node = SinglyLinkedListNode::new(value, old_head);
+        let node = SinglyLinkedListNode::<T>::new(value, old_head);
 
         self.head = Some(Box::new(node));
         self.counter += 1;
     }
 
-    pub fn pop_front(&mut self) -> Option<i32> {
+    pub fn pop_front(&mut self) -> Option<T> {
         self.head.take().map(|mut n| {
             self.head = n.next.take();
             self.counter -= 1;
@@ -68,25 +68,25 @@ mod tests {
 
     #[test]
     fn len_is_zero_for_new_list() {
-        let l = SinglyLinkedList::new();
+        let l = SinglyLinkedList::<i32>::new();
         assert_eq!(l.len(), 0);
     }
 
     #[test]
     fn is_empty_is_true_for_new_list() {
-        let l = SinglyLinkedList::new();
+        let l = SinglyLinkedList::<i32>::new();
         assert!(l.is_empty());
     }
 
     #[test]
     fn peek_front_returns_none_for_new_list() {
-        let l = SinglyLinkedList::new();
+        let l = SinglyLinkedList::<i32>::new();
         assert_eq!(l.peek_front(), Option::None);
     }
 
     #[test]
     fn push_front_increases_len() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(15);
         assert_eq!(l.len(), 1);
         l.push_front(7);
@@ -95,21 +95,21 @@ mod tests {
 
     #[test]
     fn push_front_makes_is_empty_false() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(15);
         assert!(!l.is_empty())
     }
 
     #[test]
     fn peek_front_returns_head_of_list() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(5);
         assert_eq!(l.peek_front(), Some(&5));
     }
 
     #[test]
     fn peek_front_does_not_remove_element() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(5);
         assert_eq!(l.peek_front(), Some(&5));
         assert_eq!(l.peek_front(), Some(&5));
@@ -117,7 +117,7 @@ mod tests {
 
     #[test]
     fn push_front_sets_head_as_new_value() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(8);
         assert_eq!(l.peek_front(), Some(&8));
         l.push_front(95);
@@ -126,7 +126,7 @@ mod tests {
 
     #[test]
     fn pop_front_decreases_len() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(4);
         l.push_front(9);
         assert_eq!(l.len(), 2);
@@ -138,7 +138,7 @@ mod tests {
 
     #[test]
     fn pop_front_remove_element() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(9);
         l.push_front(7);
         assert_eq!(l.peek_front(), Some(&7));
@@ -148,7 +148,7 @@ mod tests {
 
     #[test]
     fn pop_front_returns_last_pushed_front() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(1);
         l.push_front(2);
         l.push_front(3);
@@ -157,7 +157,7 @@ mod tests {
 
     #[test]
     fn pop_front_util_empty_then_none() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(1);
         l.push_front(2);
         l.push_front(3);
@@ -169,7 +169,7 @@ mod tests {
 
     #[test]
     fn clear_sets_len_to_zero() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(1);
         l.push_front(2);
         l.push_front(3);
@@ -181,7 +181,7 @@ mod tests {
 
     #[test]
     fn clear_makes_is_empty_true() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
         l.push_front(10);
 
         l.clear();
@@ -191,7 +191,7 @@ mod tests {
 
     #[test]
     fn clear_after_operations_resets_state() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
 
         l.push_front(1);
         l.push_front(2);
@@ -208,7 +208,7 @@ mod tests {
 
     #[test]
     fn alternating_push_front_and_pop_front_maintains_invariants() {
-        let mut l = SinglyLinkedList::new();
+        let mut l = SinglyLinkedList::<i32>::new();
 
         l.push_front(1);
         assert_eq!(l.peek_front(), Some(&1));
